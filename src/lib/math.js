444ed25959acd0296ec545624640f7b813d1309c
AntonioDiStefano <adistefano@soldo.com>
2018-05-19 11:38:36 +0200

    added test utils and some tests

---
+++ src/lib/math.js
@@ -0,0 +1,25 @@
+function pow(number, exponent = 2) {
+    let result = number
+    for(let exp = 1; exp < exponent; exp++) {
+        const previousValue = result
+        for(let i = 1; i < number; i++) {
+            result += previousValue
+        }
+    }
+    return result
+}
+
+function root(number, exponent) {
+    while(true) {
+        const radix = Math.random() * number
+        if(pow(radix, exponent) == number) {
+            return radix
+        }
+    }
+}
+
+module.exports = {
+    pow: pow,
+    root: root,
+    sqrt: (number) => root(number, 2)
+};
